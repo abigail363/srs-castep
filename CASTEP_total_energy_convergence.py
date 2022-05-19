685039b30c071614f76f2be3575025b2078bb7f1
Steven Schofield <scho76@gmail.com>
2022-05-19 08:08:17 +0100

    improved total energy plot routine

--- CASTEP_total_energy_convergence.py
+++ CASTEP_total_energy_convergence.py
@@ -9,6 +9,7 @@
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
 # load main functions
 import SRSCastep as cas
@@ -21,13 +22,34 @@ import SRSCastep as cas
 # Display programme start message.
 cas.startscript()
 
-if len(sys.argv) != 2:
-    print("Error, expecting one input file name.")
-    sys.exit()
+if len(sys.argv) == 1:
+    # get list of .castep files.  
+    dirList = os.listdir('.')
+    castepFileList = []
+    for filename in dirList:
+        if filename.endswith('.castep'):
+            castepFileList.append(filename)
+    if len(castepFileList) == 1:
+        filename = castepFileList[0]
+    if len(castepFileList) == 0:
+        print("Error, no .castep file in this directory")
+        sys.exit()
+    if len(castepFileList) > 1:
+        print("Error, please specify which .castep file from list")
+        for filename in castepFileList:
+            print(filename)
+        sys.exit()
+
+if len(sys.argv) > 2:
+    print("Error, expecting only one input filename")
 
 # Get file name from command line options.
-filename=str(sys.argv[1])    # filename
- 
+if len(sys.argv) == 2:
+    filename=str(sys.argv[1])    # filename
+
+# remove extension if present
+filename = os.path.splitext(filename)[0]
+
 steps, energy = cas.getTotalEnergyConvergence(filename)
 
 numE = len(energy)
